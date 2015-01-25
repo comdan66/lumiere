a8e6f846fb333b1f58c6fb6e6d25ff5088ba69d2
OA Wu <comdan66@gmail.com>
2015-01-25 16:46:28 +0800

    Fix bugs.

--- application/views/content/site/portfolios/index/content.php
+++ application/views/content/site/portfolios/index/content.php
@@ -12,7 +12,7 @@
     foreach ($portfolios as $portfolio) { ?>
       <div class='unit' data-tag="<?php echo $portfolio->tag ? $portfolio->tag->name : '';?>">
         <a href='<?php echo base_url (array ('portfolio', $portfolio->id));?>'>
-          <img src="<?php echo $portfolio->first_pic->file_name->url ('200x200');?>">
+          <img src="<?php echo $portfolio->first_pic ? $portfolio->first_pic->file_name->url ('200x200') : '';?>">
           <div class='t'><?php echo $portfolio->title;?></div>
           <div class='d'>
             <?php echo $portfolio->address;?> | <?php echo $portfolio->level;?>坪
