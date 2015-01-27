dda402555d208d398b100a238608a17b74a49186
OA Wu <comdan66@gmail.com>
2015-01-27 22:58:45 +0800

    Add about、service

---
+++ application/views/content/site/services/index/content.php
@@ -0,0 +1,159 @@
+
+  <div id="outbox">
+    
+      <div class="tbox">
+          <p class="sertitle">服務項目</p>
+            <div class="sictbox">
+              <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong01.png'));?>" width="100%" height="auto" />
+                <p class="icontitle">SCENT Space </p>
+                <p class="icncty">商業空間香氛<br/>設計、規劃、施工</p>
+            </div>
+            
+            <div class="sictbox">
+              <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong02.png'));?>" width="100%" height="auto" />
+                <p class="icontitle">Brand SCENTing</p>
+                <p class="icncty">商業空間香氛<br/>設計、規劃、施工</p>
+            </div>
+            
+            <div class="sictbox">
+              <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong03.png'));?>" width="100%" height="auto" />
+                <p class="icontitle">SCENT Formula</p>
+                <p class="icncty">商業空間香氛<br/>設計、規劃、施工</p>
+            </div>
+            
+            <div class="sictbox">
+              <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong04.png'));?>" width="100%" height="auto" />
+                <p class="icontitle">SCENT Marketing</p>
+                <p class="icncty">商業空間香氛<br/>設計、規劃、施工</p>
+            </div>
+            
+            <div class="sictbox">
+              <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong05.png'));?>" width="100%" height="auto" />
+                <p class="icontitle">Event Project</p>
+                <p class="icncty">商業空間香氛<br/>設計、規劃、施工</p>
+            </div>
+            
+            <div class="sictbox">
+              <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong06.png'));?>" width="100%" height="auto" />
+                <p class="icontitle" style="text-indent:-999px;">......</p>
+                <p class="icncty">商業空間香氛<br/>設計、規劃、施工</p>
+            </div>
+           
+           <div class="sd"></div>
+        </div>
+        
+        
+        
+        <div class="tbox">
+          <div class="scicon">
+            <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icon01.jpg'));?>" width="100%" height="100%" />
+            <p class="scictitle">產品</p>
+            </div>
+            
+            <div class="scrigbox">
+              <div class="rigbox">
+                    <div class="scbar"></div>
+                    <p class="scor">Aroma Diffuser 飯店設備等級擴香設備</p>
+                </div>
+                
+                <div class="rigbox">
+                    <div class="scbar"></div>
+                    <p class="scor">芳療師調香系列香氛</p>
+                </div>
+                
+                <div class="rigbox">
+                    <div class="scbar"></div>
+                    <p class="scor">Essential Oil 700  種以上芳療等級精油 產品化學種、產地、萃取方式多樣。</p>
+                </div>
+                
+                <div class="rigbox">
+                    <div class="scbar"></div>
+                    <p class="scor">300種以上有機認證精油</p>
+                </div>
+                
+                <div class="rigbox">
+                    <p class="ricetype">‧ ECO(法國）約204種  </p>
+          <p class="ricetype">‧ NOP(USDA 美國農業部)約172種</p>
+          <p class="ricetype">‧ ACO（澳洲）約63種</p>
+          <p class="ricetype">‧ 部分取得歐盟公平交易規章ESR,EQU</p>
+          <p class="ricetype">‧ 部分取得歐洲藥局特許供應PH規章</p>
+                </div>
+                
+                <div class="rigbox">
+                    <div class="scbar"></div>
+                    <p class="scor">超過 100種植物油及有機認證植物油</p>
+                </div>
+                
+                <div class="rigbox">
+                    <div class="scbar"></div>
+                    <p class="scor">超過 50種有機栽種有機認證純露</p>
+                </div>
+            </div>
+            
+           
+           <div class="sd"></div>
+        </div>
+        
+        
+        
+        <div class="tbox">
+            <div class="serbgimg"></div>
+           <div class="sd"></div>
+        </div>
+        
+        
+        
+        <div class="tbox">
+            <div class="scicont">
+                <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icon02.jpg'));?>" width="100%" height="100%" />
+                <p class="scictitle">收費標準</p>
+            </div>
+            
+            <div class="dbox">
+              <div class="ddddown">
+                    <div class="dddleft">
+                      <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong07.png'));?>" width="100%" height="auto" />
+                    </div>
+                    
+                    <div class="dddright">
+                      <p class="ortitle">空間大小</p>
+                        <p class="acetype">按業主目標的香氣空間大小、重點空間位置、空調位置有所不同，除面積外，挑高方式也是現場勘查量測重點</p>
+                    </div>
+                </div>
+                
+                <div class="ddddown">
+                    <div class="dddleft">
+                      <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong08.png'));?>" width="100%" height="auto" />
+                    </div>
+                    
+                    <div class="dddright">
+                      <p class="ortitle">施工方式</p>
+                        <p class="acetype">按業主室內空調配置方式施工，按中央空調 / 風扇機 / 獨立空調/ 儀器數量 / 施工方式各有不同，將於現場勘查之後確認</p>
+                    </div>
+                </div>
+                
+                <div class="ddddown">
+                    <div class="dddleft">
+                      <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong09.png'));?>" width="100%" height="auto" />
+                    </div>
+                    
+                    <div class="dddright">
+                      <p class="ortitle">儀器自有/租賃</p>
+                        <p class="acetype">採用租賃方式的客戶僅需負擔儀器租金、擁有儀器設備的客戶則不需負擔儀器租賃費用</p>
+                    </div>
+                </div>
+                
+                <div class="ddddown">
+                    <div class="dddleft">
+                      <img src="<?php echo base_url (array ('resource', 'site', 'img', 'icong10.png'));?>" width="100%" height="auto" />
+                    </div>
+                    
+                    <div class="dddright">
+                      <p class="ortitle">設計顧問費</p>
+                        <p class="acetype">針對採用量身訂作專案的客戶，採用設計師系列香氛的客戶無須負擔</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
