f161e06b1f4500cd69f53758b5242ab7b9742a05
OA Wu <comdan66@gmail.com>
2015-01-28 23:52:24 +0800

    Fix mailto.

--- application/views/frame/site/frame.php
+++ application/views/frame/site/frame.php
@@ -44,7 +44,7 @@
         <div class='footer hide'>
           <div class='t'>
             <div class='icon'>
-              <a href="aroma@lumiere.tw"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32" viewBox="0 0 32 32"><path d="M15.996 15.457l16.004-7.539v-3.918h-32v3.906zM16.004 19.879l-16.004-7.559v15.68h32v-15.656z" fill="#444444"></path></svg></a>
+              <a href="mailto:aroma@lumiere.tw"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32" viewBox="0 0 32 32"><path d="M15.996 15.457l16.004-7.539v-3.918h-32v3.906zM16.004 19.879l-16.004-7.559v15.68h32v-15.656z" fill="#444444"></path></svg></a>
             </div>
           </div>
           <div class='b'>
